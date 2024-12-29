7691c8d3e36881ac24389ba1fbb4aeac8488bbf4
Fernando Manome <fernando.manome@gmail.com>
2024-12-29 18:31:45 -0300

    Corrige a alteração de senha para uso da tabela usuário

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Support\Facades\Hash;
 
 class ResetPasswordController extends Controller
 {
@@ -21,7 +22,25 @@ class ResetPasswordController extends Controller
     use ResetsPasswords;
 
     /**
-     * Where to redirect users after resetting their password.
+     * Atualiza a senha do usuário na tabela 'usuarios'.
+     * 
+     * Este método substitui o comportamento padrão do método `setUserPassword()` 
+     * da trait `ResetsPasswords`, garantindo que a senha do usuário seja 
+     * salva no campo `senha` em vez de `password`, que não existe na tabela 
+     * `usuarios`. O método aplica o algoritmo de hash para garantir que a 
+     * senha seja armazenada de forma segura.
+     * 
+     * @param  \Illuminate\Contracts\Auth\CanResetPassword  $user  O objeto do usuário cuja senha será atualizada.
+     * @param  string  $password  A nova senha que será atribuída ao usuário.
+     * @return void
+     */
+    protected function setUserPassword($user, $password)
+    {
+        $user->senha = Hash::make($password);
+    }
+
+    /**
+     * Local onde serão redirecionados os usuários após a redefinição de senha.
      *
      * @var string
      */
