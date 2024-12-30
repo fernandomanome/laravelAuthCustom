632ba82fd10459aba2fb9c82d9e348f3a3002d15
Fernando Manome <fernando.manome@gmail.com>
2024-12-30 10:24:18 -0300

    Implmenta métodos para regras na rotina de Login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -12,23 +14,24 @@ class LoginController extends Controller
     | Login Controller
     |--------------------------------------------------------------------------
     |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
+    | Este é o controlador reponsável por lidar com a autenticação dos usuários e
+    | os redireciona para telas conforme a necessidade. O controlador utiliza um trait
+    | para fornecer funcionalidade de autenticação de forma mais conveniente.
     |
     */
 
+	/* Invoca a Trait disponibilizando as funcionalidades da lib de Login */
     use AuthenticatesUsers;
 
     /**
-     * Where to redirect users after login.
+     * Onde os usuários serão redirecionados após o login bem-sucedido.
      *
      * @var string
      */
     protected $redirectTo = '/home';
 
     /**
-     * Create a new controller instance.
+     * Cria uma nova instância do controlador.
      *
      * @return void
      */
@@ -36,5 +39,51 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
         $this->middleware('auth')->only('logout');
+    }
+
+    /**
+     * Método executado após a autenticação bem-sucedida do usuário com email e senha.
+     * 
+     * [!ATENÇÃO!] Este método pertence à Trait `AuthenticatesUsers` e está sendo sobrescrito 
+     * para permitir a execução de validações adicionais durante o processo de login,
+     * como verificações extras de permissões ou condições específicas do usuário.
+     * O objetivo é fornecer um controle mais detalhado sobre o fluxo pós-login,
+     * garantindo que o cadastro do usuário atenda a requisitos adicionais antes de prosseguir.
+     * 
+     * @param  \Illuminate\Http\Request  $request  A requisição HTTP com dados do login.
+     * @param  mixed  $user  O usuário autenticado, que pode ser um modelo de usuário ou instância personalizada.
+     * @return \Illuminate\Http\RedirectResponse  O redirecionamento após validações.
+     */
+    protected function authenticated(Request $request, $user)
+    {
+        // Realiza validações adicionais do cadastro do usuário        
+		if(!$this->validarAcessoUsuario($request, $user)) {
+			// Se não for válido redireciona o usuário
+			return redirect()->route('login')->withErrors(['email' => 'Sua conta não está ativa.']);
+		}
+        // Se o usuário estiver ativo, continua o fluxo de login
+        return redirect()->intended($this->redirectTo);
+    }
+
+    /**
+     * Método responsável por aplicar validações adicionais após a autenticação do usuário,
+     * verificando condições específicas antes de permitir o acesso ao sistema.
+     * 
+     * @param  \Illuminate\Http\Request  $request  A requisição HTTP com os dados do login.
+     * @param  mixed  $user  O usuário autenticado.
+     * @return bool Retorna se o cadastro está válido ou não.
+     */
+    private function validarAcessoUsuario(Request $request, $user) :bool
+    {
+		$cadastroUsuarioValido = isset($user->ativo) && $user->ativo ? true : false;
+
+        if (!$cadastroUsuarioValido) {
+            // Se o cadastro do usuário não atender aos requisitos, ele será desconectado e a sessão será invalidada
+            Auth::logout();
+            $request->session()->invalidate();
+			// Gera um novo token CSRF por segurança.
+            $request->session()->regenerateToken();
+        }
+		return $cadastroUsuarioValido;
     }
 }
